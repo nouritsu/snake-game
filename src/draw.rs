9354661e781020ba6ba1c9f877734cb0b2fc7eba
Aneesh <aneesh1701@gmail.com>
2022-09-18 19:03:20 +0530

    init commit

---
+++ src/draw.rs
@@ -0,0 +1,49 @@
+use piston_window::types::Color;
+use piston_window::{rectangle, Context, G2d};
+
+const BLOCK_SIZE: f64 = 25.0;
+
+pub fn to_cord(game_coord: i32) -> f64 {
+    (game_coord as f64) * BLOCK_SIZE
+}
+
+pub fn to_cord_u32(game_coord: i32) -> u32 {
+    to_cord(game_coord) as u32
+}
+
+pub fn draw_block(colour: Color, x: i32, y: i32, con: &Context, g: &mut G2d) {
+    let gui_x = to_cord(x);
+    let gui_y = to_cord(y);
+
+    rectangle(
+        colour,
+        [gui_x, gui_y, BLOCK_SIZE, BLOCK_SIZE],
+        con.transform,
+        g,
+    );
+}
+
+pub fn draw_rectangle(
+    colour: Color,
+    x: i32,
+    y: i32,
+    width: i32,
+    height: i32,
+    con: &Context,
+    g: &mut G2d,
+) {
+    let x = to_cord(x);
+    let y = to_cord(y);
+
+    rectangle(
+        colour,
+        [
+            x,
+            y,
+            BLOCK_SIZE * (width as f64),
+            BLOCK_SIZE * (height as f64),
+        ],
+        con.transform,
+        g,
+    )
+}
---
+++ src/main.rs
@@ -0,0 +1,37 @@
+extern crate piston_window;
+extern crate rand;
+
+mod draw;
+mod game;
+mod snake;
+
+use piston_window::types::Color;
+use piston_window::*;
+
+use crate::draw::to_cord_u32;
+use crate::game::Game;
+
+const BACK_COLOUR: Color = [0.5, 0.5, 0.5, 1.0];
+
+fn main() {
+    let (width, height) = (20, 20);
+
+    let mut window: PistonWindow =
+        WindowSettings::new("Snake", [to_cord_u32(width), to_cord_u32(height)])
+            .exit_on_esc(true)
+            .build()
+            .unwrap();
+
+    let mut game = Game::new(width, height);
+    while let Some(event) = window.next() {
+        if let Some(Button::Keyboard(key)) = event.press_args() {
+            game.key_pressed(key)
+        }
+        window.draw_2d(&event, |c, g, _| {
+            clear(BACK_COLOUR, g);
+            game.draw(&c, g)
+        });
+
+        event.update(|arg| game.update(arg.dt));
+    }
+}
